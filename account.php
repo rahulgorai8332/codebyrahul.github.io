a74dca2b07fdb0d53c23151cffd560ebd7ca2670
Rahul gorai <106830475+rahulgorai8332@users.noreply.github.com>
2022-09-14 11:15:32 +0530

    Update account.php

--- account.php
+++ account.php
@@ -9,7 +9,7 @@ $email_body ="User name:$name.\n".
             "User Email:$visitor_email.\n".
             "User Message:$message.\n";
 
-  $to="rahulgorai8332@.com ";
+  $to="rahulgorai8332@gmail.com ";
   $headers="From:$email_from \r\n";
   $headers .="Reply-to:$visitor_email \r\n";
   mail($to,$email_subject,$email_body,$headers);
